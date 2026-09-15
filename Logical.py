b38cf2445d272c18933d9ec7f35f552e0a8e5f0d
kartiktejani01 <kartiktejani01@gmail.com>
2026-09-15 21:53:18 +0530

    p _01

--- Logical.py
+++ Logical.py
@@ -6,8 +6,20 @@ x = 5
 
 print(x < 5 or x > 10)
 
+#Logical OP
 a = 10
 b = 15
 print("a > b and a != b:", a > b and a != b)
 print("a < b or a != b:", a < b or a != b)
 print("not(a > b):", not(a > b))
+
+#Bitwise OP
+p = 10
+Q = 4
+
+print("p & Q", p & Q)
+print("p | Q(OR)", p | Q)
+print("p ^ Q(XOR)", p ^ Q)
+print("~Q(NOT)", ~Q)
+print("p << 1", p << 1)
+print("p >> 1", p >> 1)
--- p_01.py
+++ p_01.py
@@ -1,6 +1,32 @@
-a = 10
-b = 2
+'''
+A = 5000
+R = 2
+Y = 3
 
-print("Addition:", a + b)
-print("Substraction:", a - b)
-print("Multiplication:", a * b)
+SI = (A * R * Y)/ 100
+
+CI = A * (1 + R / 100)** Y - A
+
+print("Amount of:", A)
+print("Rate:", R, "%")
+print("Time:", Y, "Year")
+print("Simple Intrest:", SI)
+print("Compund Intrest:", CI)
+'''
+length = 10
+breadth = 5
+
+rectangle_area = length * breadth
+rectangle_primeter = 2 * (length + breadth)
+
+print("rectangle_area:", rectangle_area)
+print("rectangle_primeter", rectangle_primeter)
+
+# circle
+radius = 7
+
+circle_area = 3.14 * radius * radius
+circle_primeter = 2 * 3.14 * radius
+
+print("circle_area",circle_area)
+print("circle_primeter",circle_primeter)
